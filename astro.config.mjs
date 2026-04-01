22c9c69c022564011ceba10659bd961e024109ae
Calypso <jesuiscalypso@proton.me>
2026-03-31 21:11:34 -0400

    quick and dirty initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, fontProviders } from 'astro/config';
+import icon from 'astro-icon'
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        icon()
+    ],
+    fonts: [{
+        provider: fontProviders.fontsource(),
+        name: "Roboto",
+        cssVariable: "--font-roboto",
+        weights: [100, 200, 300, 400, 500, 600],
+        styles: ['normal']
+    }, {
+        provider: fontProviders.fontsource(),
+        name: "Abril Fatface",
+        cssVariable: "--font-abril-fatface",
+        weights: ['200', '400']
+    }]
+});
